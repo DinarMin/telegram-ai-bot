f43015149760c24e0778fa15fa40a3d46c0efd3b
Dinar2202 <dinar2202@gmail.com>
2025-05-21 19:30:37 +0300

    first commit

---
+++ dist/ai.js
@@ -0,0 +1,21 @@
+export async function getAIRepsonse(message) {
+    const response = await fetch("http://10.8.0.2:1234/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: "nous-hermes-q8/nous",
+            messages: [
+                { role: "system", content: "Ты дружелюбный помощник." },
+                { role: "user", content: message },
+            ],
+            temperature: 0.7,
+        }),
+    });
+    if (!response.ok) {
+        throw new Error(`Ошибка от LM Studio: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.choices[0].message.content;
+}
---
+++ src/bot.ts
@@ -0,0 +1,45 @@
+import { Telegraf, Context } from "telegraf";
+import { getAIRepsonse } from "./ai.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const token = process.env.TOKEN;
+if (!token) {
+  throw new Error("BOT_TOKEN is not set in environment variables.");
+}
+
+const bot = new Telegraf(token);
+
+bot.start(async (ctx: Context) => {
+  await ctx.reply("Привет! Я твоя сучка и готова отвечать на вопросы!))");
+  await ctx.reply("Задавай по скорее, я буду вежлива с тобой)");
+  await ctx.reply("Мяяу)) Если нужна доп. информация пиши '/help'");
+});
+
+bot.help((ctx: Context) => {
+  ctx.reply('AI - модель: nous-Q8. Для связи с разработчиком пишите в tg: @dinar2202 ')
+});
+
+bot.on("text", async (ctx: Context) => {
+  try {
+    if (!ctx.message || !("text" in ctx.message)) {
+      await ctx.reply("Я понимаю только текстовые сообщения.");
+      return;
+    }
+    const userMessage = ctx.message.text;
+    const thinkingMsg = await ctx.reply("Формулирую мысли...");
+
+    const aiReply = await getAIRepsonse(userMessage);
+
+    await ctx.deleteMessage(thinkingMsg.message_id);
+    await ctx.reply(aiReply);
+  } catch (err) {
+    console.error("Ошибка:", err);
+    await ctx.reply("Ошибка при обращении к ИИ 😢");
+  }
+});
+
+
+bot.launch();
+console.log("Бот запущен!");
